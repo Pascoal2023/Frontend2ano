da10cc2fcebbde48a88bde3297c267b5947ce661
Pascoal2023 <145348009+Pascoal2023@users.noreply.github.com>
2023-09-18 14:49:15 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,17 @@
+console.log(`Trabalhando com listas`);
+const listaDeDestinos = new Array (
+    `Loanda`,
+    `Porto Rico`,
+    `Maringá`
+);
+console.log(`Destinos possíveis`);
+console.log(listaDeDestinos);
+//listaDeDestinos.push(`Curitiba`) // adicionando um item na lista
+console.log("Destinos possíveis:");
+
+console.log(listaDeDestinos);
+
+//listaDeDestinos.splice(1,1);
+//console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
